3a71579796c19f51ae400541f776975be99aa60a
Gustavo-tech <gustavosh31@gmail.com>
2021-02-22 13:38:55 -0300

    Finishing first day

--- moveit/src/App.tsx
+++ moveit/src/App.tsx
@@ -1,8 +1,11 @@
+import ExperienceBar from './components/ExperienceBar';
 import './styles/global.css';
 
 function App() {
   return (
-    <h2>Testings</h2>
+    <div className="container">
+      <ExperienceBar />
+    </div>
   );
 }
 
