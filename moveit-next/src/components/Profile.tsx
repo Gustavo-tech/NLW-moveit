6ba4f5c25c1defaab3a92bbfed83546b8b620d17
Gustavo-tech <gustavosh31@gmail.com>
2021-02-23 22:48:26 -0300

    Importing styles

---
+++ moveit-next/src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+  return (
+    <div>
+      <img src="https://github.com/Gustavo-tech.png" alt="Gustavo Henrique"/>
+      <div>
+        <strong>Gustavo Henrique</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  );
+}
